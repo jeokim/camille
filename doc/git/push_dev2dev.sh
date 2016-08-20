61910ad907628ff24d32878b86453737e9ce6de2
jeokim <jeokim@certainty-a.local>
2016-08-19 17:29:42 -0700

    updated

--- doc/git/push_dev2dev.sh
+++ doc/git/push_dev2dev.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 if [ $# -ne 1 ]
 then
-  echo "... ERROR: specify the commit message."
-  exit 1
+  echo "... WARNING: specify a commit message."
+  MSG="updated"
+else
+  MSG=$1 
 fi
 #
 git add *
 git status
-git commit -a -m "$1"
+git commit -a -m "$MSG"
 git remote add github git@github.com:jeokim/camille.git
 git push github dev
