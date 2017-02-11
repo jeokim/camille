5fa5df2817fc3d57fa5ad9ef9facf130fd0ba4dc
jeokim <jeokim@Jeonglaes-MacBook-Pro.local>
2017-02-11 00:02:02 -0800

    updated

--- src/io/probe.cpp
+++ src/io/probe.cpp
@@ -9,8 +9,32 @@ namespace probe {
 
 void initialize(UserInput *myinput) {
 
-  MESSAGE_STDOUT("Hi, my name is Youngchul Kim.");
-  mpi::graceful_exit("See you later!");
+  if (myinput->do_probe == FALSE)
+    return;
+  assert(myinput->num_probes > 0);
+
+
+
+
+
+
+
+
+
+
+
+//     for (int iprobe = 0; iprobe < num_probes; iprobe++) {
+//       inputDeck::get_userInput("PROBE","NAME",tmp_probe_name[iprobe],iprobe);
+//       inputDeck::get_userInput("PROBE","INTERVAL",tmp_probe_interval[iprobe],iprobe);
+//       inputDeck::get_userInput("PROBE","XYZ",DIM_MAX,xyz,iprobe);
+//       for (int idir = XDIR; idir < DIM_MAX; idir++)
+//         tmp_probe_xyz[iprobe][idir] = xyz[idir];
+//     } // iprobe
+
+  // clean up temporary storage  
+  DEALLOCATE_1DPTR(myinput->tmp_probe_name);
+  DEALLOCATE_1DPTR(myinput->tmp_probe_interval);
+  DEALLOCATE_2DPTR(myinput->tmp_probe_xyz, num_probes);
 
   return;
 
