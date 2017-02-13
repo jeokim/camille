d0ce3a7422929967ea264d9f34541e846975496f
jeokim <jeokim@Jeonglaes-MacBook-Pro.local>
2017-02-12 21:52:32 -0800

    updated

--- src/io/probe.cpp
+++ src/io/probe.cpp
@@ -23,11 +23,11 @@ void initialize(UserInput *myinput, Geometry::StructuredGrid *mygrid) {
   std::string str_output;
 
   num_myprobes = 0; // no probe for now
-MESSAGE_STDOUT("FIRST");
+
   // core2probe[k] = 1 if this core owns the probe k (k = 0, 1, 2, ...)
   int *core2probe;
   ALLOCATE1D_INT_1ARG(core2probe, myinput->num_probes);
-MESSAGE_STDOUT("SECOND");
+
   // go over all probes
   ALLOCATE1D_INT_1ARG(corresponding_ijk, DIM_MAX);
   for (int iprobe = 0; iprobe < myinput->num_probes; iprobe++) {
@@ -37,6 +37,7 @@ MESSAGE_STDOUT("SECOND");
     for (int idir = XDIR; idir < DIM_MAX; idir++)
       xyz[idir] = myinput->tmp_probe_xyz[iprobe][idir];
 
+std::cout << "Rank (before): " << mpi::irank << std::endl;
     // search
     if (mygrid->check_if_this_is_my_point(myinput->num_dim, xyz, corresponding_ijk) == TRUE) {
 
@@ -47,6 +48,7 @@ MESSAGE_STDOUT("SECOND");
 
     } // mygrid->check_if_this_is_my_point(myinput->num_dim, xyz, corresponding_ijk)
   } // iprobe
+std::cout << "Rank (after): " << mpi::irank << std::endl;
   DEALLOCATE_1DPTR(corresponding_ijk);
 MESSAGE_STDOUT("THIRD");
   // due to grid overlapping (either ghost cell or overset), a single probe could be claimed by more than one grid
