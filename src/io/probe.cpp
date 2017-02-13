af79b2172ddf243e94b81be616b96eed3a05ea54
jeokim <jeokim@Jeonglaes-MacBook-Pro.local>
2017-02-13 00:33:33 -0800

    updated

--- src/io/probe.cpp
+++ src/io/probe.cpp
@@ -52,6 +52,7 @@ void initialize(UserInput *myinput, Geometry::StructuredGrid *mygrid) {
   // due to grid overlapping (either ghost cell or overset), a single probe could be claimed by more than one grid
 for (int iprobe = 0; iprobe < myinput->num_probes; iprobe++)
 std::cout << "Rank: " << mpi::irank << ", probe: " << iprobe << ", num probes: " << num_myprobes << ", core2probe: " << core2probe[iprobe] << std::endl;
+mpi::graceful_exit("bye!");
   int *core2probe_sum;
   ALLOCATE1D_INT_1ARG(core2probe_sum, myinput->num_probes);
   MPI_Allreduce(core2probe, core2probe_sum, myinput->num_probes, MPI_INT, MPI_SUM, mpi::comm_region);
