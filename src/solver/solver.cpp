6043a45912745e54f51d3c5ec426121c3363fc75
jeokim <jeokim@DN0a225868.SUNet>
2017-02-15 16:40:19 -0800

    updated

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -109,7 +109,7 @@ ofs.close();
 l0 = mygrid->idx1D(108-1, 0, 0);
 wplus = mystate->sol[IVAR_P][l0]/(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]) + 
         mystate->sol[IVAR_UX][l0]/sqrt(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]/mystate->sol_aux[IAUX_RHO_MEAN][l0]);
-double ws = mystate->sol[IVAR_S][l0]/mystate->sol_aux[IAUX_CP][l0];
+ws = mystate->sol[IVAR_S][l0]/mystate->sol_aux[IAUX_CP][l0];
 ofs.open("outlet_wplus.dat", std::ofstream::app);
 ofs << std::setw(16) << mygrid->cell[l0].xyz[XDIR] << " "
     << std::setw(16) << temporal::time_sol << " "
