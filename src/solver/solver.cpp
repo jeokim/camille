468b7605692eac92b1b50d988ffdb0769b9e611d
jeokim <jeokim@DN0a22455b.SUNet>
2017-02-16 16:13:38 -0800

    updated

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -89,7 +89,7 @@ std::ofstream ofs;
 int l0;
 double pPrime, uPrime, wplus, wminus, ws, wZ;
 //
-l0 = mygrid->idx1D(8-1, 0, 0);
+l0 = mygrid->idx1D(20-1, 0, 0);
 pPrime = mystate->sol[IVAR_P][l0]/(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]);
 uPrime = mystate->sol[IVAR_UX][l0]/sqrt(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]/mystate->sol_aux[IAUX_RHO_MEAN][l0]);
 wplus = pPrime + uPrime;
