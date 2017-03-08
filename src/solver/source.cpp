61029e4940f3a652a00e6f7e8d1a1c0fa3463864
jeokim <jeokim@Jeonglaes-MacBook-Pro.local>
2017-03-07 21:16:34 -0800

    updated

--- src/solver/source.cpp
+++ src/solver/source.cpp
@@ -99,6 +99,23 @@ void apply_physicalSource(UserInput *myinput, Geometry::StructuredGrid *mygrid, 
     {
     }
 
+  else if (mystate->simulation == "CASE_LINEAR_NOZZLE")
+    {
+      // non-dimensionalize
+      double amplitude = 4.6146e-04;
+      double frequency = 1.0 / 0.5042;
+      double Bx = 4.0;
+
+      double angular_frequency = frequency * math_constants::twopi;
+      double amp_sin_wt = amplitude * sin(angular_frequency * time);
+      for (int l0 = 0; l0 < num_samples; l0++) {
+
+        double xloc = mygrid->cell[l0].xyz[XDIR] - 0.0;
+        (rhs[IVAR_P])[l0] += exp(-(Bx * xloc*xloc) * amp_sin_wt;
+
+      } // l0
+    }
+
   else
     // do nothing since simulations may not have any source
 
