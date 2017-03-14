473e84b3583ec032c13e07b68eb5209901a7af5c
jeokim <jeokim@DN0a224964.SUNet>
2017-03-13 19:27:43 -0700

    updated

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -89,13 +89,13 @@ std::ofstream ofs;
 int l0;
 double pPrime, uPrime, wplus, wminus, ws, wZ;
 //
-l0 = mygrid->idx1D(20, 0, 0);
+l0 = mygrid->idx1D(0, 0, 0);
 pPrime = mystate->sol[IVAR_P][l0]/(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]);
 uPrime = mystate->sol[IVAR_UX][l0]/sqrt(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]/mystate->sol_aux[IAUX_RHO_MEAN][l0]);
 wplus = pPrime + uPrime;
 wminus = pPrime - uPrime;
 ws = mystate->sol[IVAR_S][l0]/(mystate->sol_aux[IAUX_CP])[l0];
-//wZ = mystate->sol[IVAR_Z][l0];
+wZ = mystate->sol[IVAR_Z][l0];
 if (mpi::irank == 0) { 
 ofs.open("inlet_wplus.dat", std::ofstream::app);
 ofs << std::setw(16) << mygrid->cell[l0].xyz[XDIR] << " "
@@ -203,13 +203,13 @@ ofs.close();
 //ofs.close();
 //} // mpi::irank
 //
-l0 = mygrid->idx1D(mygrid->ie[XI]-20, 0, 0);
+l0 = mygrid->idx1D(mygrid->ie[XI]-0, 0, 0);
 pPrime = mystate->sol[IVAR_P][l0]/(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]);
 uPrime = mystate->sol[IVAR_UX][l0]/sqrt(myinput->gamma_specificheat*mystate->sol_mean[IVAR_P][l0]/mystate->sol_aux[IAUX_RHO_MEAN][l0]);
 wplus = pPrime + uPrime;
 wminus = pPrime - uPrime;
 ws = mystate->sol[IVAR_S][l0]/(mystate->sol_aux[IAUX_CP])[l0];
-//wZ = mystate->sol[IVAR_Z][l0];
+wZ = mystate->sol[IVAR_Z][l0];
 if (mpi::irank == mpi::nprocs-1) { 
 ofs.open("outlet_wplus.dat", std::ofstream::app);
 ofs << std::setw(16) << mygrid->cell[l0].xyz[XDIR] << " "
