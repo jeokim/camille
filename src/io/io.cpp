85da02e9a61949af5f161614d7972b97fea895a6
jeokim <jeokim@DN0a22587e.SUNet>
2017-11-14 17:47:59 -0800

    updated

--- src/io/io.cpp
+++ src/io/io.cpp
@@ -1126,7 +1126,7 @@ void read_bc(UserInput *myinput, Geometry::StructuredGrid *mygrid, Geometry::Str
 
 
   for (int ibc = FIRST; ibc < num_boundaryCondition_nonperiodic; ibc++) {
-std::cout << mpi::irank << ", " << (mygrid->boundaryCondition[ibc]).itype << std::endl;
+std::cout << "Rank: " << mpi::irank << ", " << (mygrid->boundaryCondition[ibc]).which_boundary << std::endl;
 
   } // ibc
 mpi::wait_allothers();
