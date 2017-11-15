ef5b917c4b033f829003f4df9aea7ba938bbb21d
jeokim <jeokim@DN0a224251.SUNet>
2017-11-15 15:16:06 -0800

    updated

--- src/solver/boundary_condition.cpp
+++ src/solver/boundary_condition.cpp
@@ -564,7 +564,7 @@ void bc_dirichlet_file(Geometry::StructuredBoundaryCondition *myboundary, Geomet
 
             // interpolate in time
             if (myinput->OA_time_inflow == 4) {
-idx_time_inflow_file=1;
+idx_time_inflow_file=2;
               int *idx;
               ALLOCATE1D_INT_1ARG(idx,myinput->OA_time_inflow+1);
               for (int i = FIRST; i < myinput->OA_time_inflow+1; i++) {
