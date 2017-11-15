e6a895267e22df3eed805a0453cb36c670e9659f
jeokim <jeokim@DN0a224251.SUNet>
2017-11-15 12:24:13 -0800

    updated

--- src/core/simulation.cpp
+++ src/core/simulation.cpp
@@ -200,9 +200,9 @@ void initialize(int argc, char * argv[]) {
   io::read_inflow(&myinput, grid); // read from the inflow data file
   mpi::wait_allothers("Boundary and inflow are initialized.");
 std::cout << "Rank: " << mpi::irank << "; " << io::num_samples_extern << "; " << io::period_samples_extern;
-std::cout << ": " << time_extern[FIRST];
+std::cout << ": " << io::time_extern[FIRST];
 for (int ivar = FIRST; ivar < myinput->num_vars_sol; ivar++)
-std::cout << ", " << sol_extern[ivar][FIRST];
+std::cout << ", " << io::sol_extern[ivar][FIRST];
 mpi::graceful_exit("!!!");l
 
 
